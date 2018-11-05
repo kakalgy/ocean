6538d70c7984d29e9cf73d45a733b4bcab52751c
kakalgy <kaka_lgy@163.com>
2018-11-05 17:36:54 +0800

    修改OperatorStateMain

--- starfish/src/main/java/com/yusj/starfish/entry/OperatorStateMain.java
+++ starfish/src/main/java/com/yusj/starfish/entry/OperatorStateMain.java
@@ -4,6 +4,7 @@ import com.yusj.starfish.map.CountWithOperatorStateFlatMap;
 import com.yusj.starfish.source.SocketSource;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
@@ -29,12 +30,17 @@ public class OperatorStateMain {
 //        });
 
         //使用Socket作为数据源
-        SocketSource socketSource = new SocketSource(args);
-        env.socketTextStream(socketSource.getHostName(), socketSource.getPort(), "\n").map(new MapFunction<String, Long>() {
-            public Long map(String s) throws Exception {
-                return Long.parseLong(s);
-            }
-        }).flatMap(new CountWithOperatorStateFlatMap()).addSink(new SinkFunction<String>() {
+//        SocketSource socketSource = new SocketSource(args);
+//        DataStream<Long> data = env.socketTextStream(socketSource.getHostName(), socketSource.getPort(), "\n").map(new MapFunction<String, Long>() {
+//            public Long map(String s) throws Exception {
+//                return Long.parseLong(s);
+//            }
+//        });
+
+        //使用测试数据作为数据源
+        DataStream<Long> data = env.fromElements(1L, 2L, 3L, 4L, 1L, 10L, 6L, 4L, 1L, 3L, 2L, 1L);
+
+        data.flatMap(new CountWithOperatorStateFlatMap()).addSink(new SinkFunction<String>() {
             public void invoke(String value, Context context) throws Exception {
                 System.out.println("sink result: " + value);
             }
